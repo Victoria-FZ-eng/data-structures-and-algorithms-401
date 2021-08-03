ebf5b1f67ccdf381340e1895c8a1440d63ae15ce
Victoria-FZ-eng <vzulof@gmail.com>
2021-08-03 09:58:16 +0300

    Initializing

---
+++ cc-08/app/src/main/java/cc/LinkedList.java
@@ -0,0 +1,43 @@
+package cc;
+
+import java.util.*;
+
+public class LinkedList {
+
+    Node head;
+    Node tail;
+
+    // adding node to the end
+    public String append(int value){
+        // add Node to the end
+        Node node = new Node(value);
+        if(head == null){
+            head = node;
+            tail = head;
+        }
+        else{
+            tail.next = node;
+            tail = node;
+        }
+        return "Value Added";
+    }
+
+    // getting nodes as an array of integers
+    public ArrayList<Integer> getValues(){
+        ArrayList<Integer> vals = new ArrayList<Integer>();
+        Node current = head;
+        while(current != null){
+            vals.add(current.data);
+            current = current.next;
+        }
+
+        //System.out.println(vals);
+        return vals;
+    }
+
+    //---------------------------------------------------------------------------------------
+    // below is the method for code challenge 08
+    //---------------------------------------------------------------------------------------
+
+    
+}
