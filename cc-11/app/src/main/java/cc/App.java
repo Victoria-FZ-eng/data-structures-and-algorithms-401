44913707990a28aa2ab80e05edef9447989bf162
Victoria-FZ-eng <vzulof@gmail.com>
2021-08-08 20:28:33 +0300

    methods

--- cc-11/app/src/main/java/cc/App.java
+++ cc-11/app/src/main/java/cc/App.java
@@ -5,10 +5,31 @@ package cc;
 
 public class App {
     public String getGreeting() {
-        return "Hello World!";
+        return "Hello From Code Challenge 11";
     }
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception {
         System.out.println(new App().getGreeting());
+
+        PseudoQueue pseudoQueue= new PseudoQueue();
+        pseudoQueue.enqueue(1);
+        pseudoQueue.enqueue(2);
+        pseudoQueue.enqueue(3);
+        System.out.println(enQ(pseudoQueue,4));
+        System.out.println(deQ(pseudoQueue));
+        pseudoQueue.displayQueue();
+        System.out.println(pseudoQueue.displayQueue());
+
+    }
+
+    public static String enQ(PseudoQueue pq, int num){
+       pq.enqueue(num);
+        System.out.println(pq.displayQueue());
+       return pq.displayQueue();
+    }
+
+    public static String deQ(PseudoQueue pq) throws Exception {
+
+        return pq.dequeue();
     }
 }
---
+++ cc-11/app/src/main/java/cc/Node.java
@@ -0,0 +1,15 @@
+package cc;
+
+public class Node<T> {
+
+    public T data;
+    Node<T> next;
+   // Node<T> previous;
+
+
+    public Node(T value) {
+
+        data = value;
+    }
+
+}
