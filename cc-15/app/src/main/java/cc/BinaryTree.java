56ac1a71c914416203eb934b221c3e4042c21f9b
Victoria-FZ-eng <vzulof@gmail.com>
2021-08-14 14:53:04 +0300

    tree implementing

--- cc-15/app/src/main/java/cc/BinaryTree.java
+++ cc-15/app/src/main/java/cc/BinaryTree.java
@@ -4,49 +4,60 @@ import java.util.ArrayList;
 
 public class BinaryTree<T> {
 
-Node<T> root;
+    Node<T> root;
 
-static ArrayList<Object> preOrderList = new ArrayList<Object>();
-static ArrayList<Object> inOrderList = new ArrayList<>();
-static ArrayList<Object> postOrderList = new ArrayList<>();
-
-
-public static ArrayList<Object> preOrder(Node root){
-
-    if(root != null){
-        preOrderList.add(root.data);
+    BinaryTree(T value)
+    {
+        root = new Node(value);
     }
-    if (root.left != null){
-        preOrder(root.left);
-    }
-    if(root.right != null){
-        preOrder(root.right);
-    }
-    return preOrderList;
-}
 
-public static ArrayList<Object> inOrder (Node root){
+    BinaryTree()
+    {
+        root = null;
+    }
 
-    if(root.left !=null){
-        inOrder(root.left);
-        inOrderList.add(root.left.data);
-    }
-    if(root.right !=null){
-        inOrder(root.right);
-    }
-    return  inOrderList;
-}
 
-public static ArrayList<Object> postOrder(Node root){
-    if(root.left != null){
-        postOrder(root.left);
-    }
-    if(root.right !=null){
-        postOrder(root.right);
-    }
-    postOrderList.add(root.data);
+    static ArrayList<Object> preOrderList = new ArrayList<Object>();
+    static ArrayList<Object> inOrderList = new ArrayList<>();
+    static ArrayList<Object> postOrderList = new ArrayList<>();
 
-    return postOrderList;
-}
+
+
+    public ArrayList<Object> preOrder(Node root) {
+
+        if (root != null) {
+            preOrderList.add(root.data);
+        }
+        if (root.left != null) {
+            preOrder(root.left);
+        }
+        if (root.right != null) {
+            preOrder(root.right);
+        }
+        return preOrderList;
+    }
+    public static ArrayList<Object> inOrder (Node root){
+
+        if(root.left !=null){
+            inOrder(root.left);
+            inOrderList.add(root.left.data);
+        }
+        if(root.right !=null){
+            inOrder(root.right);
+        }
+        return  inOrderList;
+    }
+
+    public static ArrayList<Object> postOrder(Node root){
+        if(root.left != null){
+            postOrder(root.left);
+        }
+        if(root.right !=null){
+            postOrder(root.right);
+        }
+        postOrderList.add(root.data);
+
+        return postOrderList;
+    }
 
 }
