1985b6f5451ecdac34f43b30f1ccde9831edc70d
Victoria-FZ-eng <vzulof@gmail.com>
2021-08-14 13:22:35 +0300

    traverse

--- cc-15/app/src/main/java/cc/BinaryTree.java
+++ cc-15/app/src/main/java/cc/BinaryTree.java
@@ -1,5 +1,52 @@
 package cc;
 
-public class BinaryTree {
-    
+import java.util.ArrayList;
+
+public class BinaryTree<T> {
+
+Node<T> root;
+
+static ArrayList<Object> preOrderList = new ArrayList<Object>();
+static ArrayList<Object> inOrderList = new ArrayList<>();
+static ArrayList<Object> postOrderList = new ArrayList<>();
+
+
+public static ArrayList<Object> preOrder(Node root){
+
+    if(root != null){
+        preOrderList.add(root.data);
+    }
+    if (root.left != null){
+        preOrder(root.left);
+    }
+    if(root.right != null){
+        preOrder(root.right);
+    }
+    return preOrderList;
+}
+
+public static ArrayList<Object> inOrder (Node root){
+
+    if(root.left !=null){
+        inOrder(root.left);
+        inOrderList.add(root.left.data);
+    }
+    if(root.right !=null){
+        inOrder(root.right);
+    }
+    return  inOrderList;
+}
+
+public static ArrayList<Object> postOrder(Node root){
+    if(root.left != null){
+        postOrder(root.left);
+    }
+    if(root.right !=null){
+        postOrder(root.right);
+    }
+    postOrderList.add(root.data);
+
+    return postOrderList;
+}
+
 }
