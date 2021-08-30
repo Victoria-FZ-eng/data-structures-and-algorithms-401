0dbebb816f9aee4db7f95d44c14067b674b760d4
Victoria-FZ-eng <vzulof@gmail.com>
2021-08-30 19:54:50 +0300

    method

--- part-2/cc-27/app/src/main/java/cc/App.java
+++ part-2/cc-27/app/src/main/java/cc/App.java
@@ -10,5 +10,21 @@ public class App {
 
     public static void main(String[] args) {
         System.out.println(new App().getGreeting());
+
+        int arr1[] = {8,4,23,42,16,15};
+        sortArr(arr1);
+        int arr2[] = {20,18,12,8,5,-2};
+        sortArr(arr2);
+        int arr3[] = {5,12,7,5,5,7};
+        sortArr(arr3);
+        int arr4[] = {2,3,5,7,13,11};
+        sortArr(arr4);
+    }
+
+    public static void sortArr(int arr[]){
+        MergeSort ms = new MergeSort();
+
+        ms.sort(arr);
+        ms.printArray(arr);
     }
 }
--- part-2/cc-27/app/src/main/java/cc/MergeSort.java
+++ part-2/cc-27/app/src/main/java/cc/MergeSort.java
@@ -1,4 +1,70 @@
 package cc;
 
 public class MergeSort {
+
+    static void sort(int arr[]){
+
+        int n = arr.length;
+
+        if(n > 1 ){
+            int mid = n/2;
+            int leftArr[]= new int[mid];
+            int rightArr[] = new int [n-mid];
+
+            for (int i = 0; i < mid; i++) {
+                leftArr[i]= arr[i];
+
+            }
+
+
+            for (int i = 0; i < (n-mid) ; i++) {
+                rightArr[i]=arr[mid+i];
+            }
+
+            sort(leftArr);
+            sort(rightArr);
+
+            merge(leftArr,rightArr,arr);
+           
+        }
+
+    }
+
+    static void merge(int leftArr[],int rightArr[], int arr[] ){
+        int i = 0;
+        int j = 0;
+        int k = 0;
+
+        while(i < leftArr.length && j < rightArr.length){
+            if(leftArr[i] <= rightArr[j]){
+                arr[k]= leftArr[i];
+                i++;
+            }else{
+                arr[k]= rightArr[j];
+                j++;
+            }
+            k++;
+        }
+
+
+        if(i == leftArr.length){
+            for(int c=k; c<arr.length; c++,j++) {
+                arr[c] = rightArr[j];
+            }
+        }else{
+            for(int c=k; c<arr.length; c++,i++) {
+                arr[c] = leftArr[i];
+            }
+        }
+    }
+
+
+    static void printArray(int arr[])
+    {
+        int n = arr.length;
+        for (int i = 0; i < n; ++i)
+            System.out.print(arr[i] + " ");
+
+        System.out.println();
+    }
 }
