1b1ad223d65dc5daf958a530644e20bade211293
Victoria-FZ-eng <vzulof@gmail.com>
2021-09-04 13:55:25 +0300

    done with 4 methods, add-get-hash-contain

--- part-2/cc-30/app/src/main/java/cc/App.java
+++ part-2/cc-30/app/src/main/java/cc/App.java
@@ -10,5 +10,16 @@ public class App {
 
     public static void main(String[] args) {
         System.out.println(new App().getGreeting());
+
+//        Map<String, Integer> map = new Map<>();
+//        map.add("this", 1);
+//        map.add("coder", 2);
+//        map.add("this", 4);
+//        map.add("hi", 5);
+//        System.out.println(map.size());
+//        System.out.println(map.remove("this"));
+//        System.out.println(map.remove("this"));
+//        System.out.println(map.size());
+//        System.out.println(map.isEmpty());
     }
 }
--- part-2/cc-30/app/src/main/java/cc/Map.java
+++ part-2/cc-30/app/src/main/java/cc/Map.java
@@ -32,8 +32,7 @@ public class Map<K, V> {
     }
 
     // get the index of in the collection for the key
-    private int hash(K key)
-    {
+    private int hash(K key) {
         int hashCode = hashCode(key);
         int index = hashCode % numBuckets;
         // key.hashCode() coule be negative.
@@ -41,7 +40,68 @@ public class Map<K, V> {
         return index;
     }
 
-    // to remove key, value pair
+    // Returns value for a key
+    public V get(K key) {
+        int bucketIndex = hash(key);
+        int hashCode = hashCode(key);
+
+        HashNode<K, V> head = bucketArray.get(bucketIndex);
+
+        while (head != null) {
+            if (head.key.equals(key) && head.hashCode == hashCode)
+                return head.value;
+            head = head.next;
+        }
+        return null;
+    }
+
+    // Adds a key value pair to hash-table
+    public void add(K key, V value) {
+
+        int bucketIndex = hash(key);
+        int hashCode = hashCode(key);
+        HashNode<K, V> head = bucketArray.get(bucketIndex);
+
+
+        while (head != null) {
+            if (head.key.equals(key) && head.hashCode == hashCode) {
+                head.value = value;
+                return;
+            }
+            head = head.next;
+        }
+
+
+        size++;
+        head = bucketArray.get(bucketIndex);
+        HashNode<K, V> newNode
+                = new HashNode<K, V>(key, value, hashCode);
+        newNode.next = head;
+        bucketArray.set(bucketIndex, newNode);
+
+        // If load factor goes beyond threshold, then
+        // double hash table size
+        if ((1.0 * size) / numBuckets >= 0.7) {
+            ArrayList<HashNode<K, V> > temp = bucketArray;
+            bucketArray = new ArrayList<>();
+            numBuckets = 2 * numBuckets;
+            size = 0;
+            for (int i = 0; i < numBuckets; i++)
+                bucketArray.add(null);
+
+            for (HashNode<K, V> headNode : temp) {
+                while (headNode != null) {
+                    add(headNode.key, headNode.value);
+                    headNode = headNode.next;
+                }
+            }
+        }
+    }
+
+
+
+
+    // to remove key,value pair - not required in cc-30
     public V remove(K key) {
         int bucketIndex = hash(key);
         int hashCode = hashCode(key);
@@ -59,5 +119,22 @@ public class Map<K, V> {
             prev = head;
             head = head.next;
         }
+
+        // If key was not there
+        if (head == null)
+            return null;
+
+        // Reduce size
+        size--;
+
+        // Remove key
+        if (prev != null)
+            prev.next = head.next;
+        else
+            bucketArray.set(bucketIndex, head.next);
+
+        return head.value;
+    }
+
 
 }
