2d3938c2f1ac3260438002a65e008d039a234b35
Victoria-FZ-eng <vzulof@gmail.com>
2021-09-05 20:11:11 +0300

    getting the hashmap implementation

--- part-2/cc-31/app/src/main/java/cc/App.java
+++ part-2/cc-31/app/src/main/java/cc/App.java
@@ -3,12 +3,25 @@
  */
 package cc;
 
+import java.util.ArrayList;
+
 public class App {
     public String getGreeting() {
-        return "Hello World!";
+        return "Hello From Code Challenge 31!";
     }
 
     public static void main(String[] args) {
         System.out.println(new App().getGreeting());
+
+        String s = "This is a sample sentence.";
+        String[] words = s.split("\\s+");
+        for (int i = 0; i < words.length; i++) {
+            // You may want to check for a non-word character before blindly
+            // performing a replacement
+            // It may also be necessary to adjust the character class
+            words[i] = words[i].replaceAll("[^\\w]", "");
+            System.out.println(words[i]);
+        }
+
     }
 }
---
+++ part-2/cc-31/read31.md
@@ -0,0 +1,15 @@
+# Challenge Summary
+<!-- Description of the challenge -->
+
+## Whiteboard Process
+<!-- Embedded whiteboard image -->
+
+## Approach & Efficiency
+<!-- What approach did you take? Why? What is the Big O space/time for this approach? -->
+
+## Solution
+<!-- Show how to run your code, and examples of it in action -->
+
+
+**References:**
+* [splitting string into array of words](https://stackoverflow.com/questions/4674850/converting-a-sentence-string-to-a-string-array-of-words-in-java)
