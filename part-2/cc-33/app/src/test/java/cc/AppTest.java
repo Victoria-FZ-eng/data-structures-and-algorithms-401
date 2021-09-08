837b7ab443341b96819ebf9f78f45a9b928b4fd9
Victoria-FZ-eng <vzulof@gmail.com>
2021-09-08 16:33:57 +0300

    test

--- part-2/cc-33/app/src/test/java/cc/AppTest.java
+++ part-2/cc-33/app/src/test/java/cc/AppTest.java
@@ -11,4 +11,23 @@ class AppTest {
         App classUnderTest = new App();
         assertNotNull(classUnderTest.getGreeting(), "app should have a greeting");
     }
+    @Test void testLeftJoins(){
+
+        Map map1 = new Map();
+        map1.add("fond","enamored");
+        map1.add("wrath","anger");
+        map1.add("diligent","employed");
+        map1.add("outfit","grab");
+        map1.add("guid","usher");
+
+        Map map2 = new Map();
+        map2.add("fond","averse");
+        map2.add("wrath","delight");
+        map2.add("diligent","idle");
+        map2.add("guid","follow");
+        map2.add("flow","jam");
+
+        assertEquals("[[diligent, employed, idle], [fond, enamored, averse], [guid, usher, follow], [outfit, grab, null], [diligent, employed, idle]]",
+             String.valueOf(App.leftJoins(map1,map2))   );
+    }
 }
